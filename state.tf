a6be7c6520a5011a7e5b77f43b2c5706d31b93ae
kcsolution <43558535+kcsolution@users.noreply.github.com>
2021-06-21 08:30:47 -0300

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "kcsolutionHackatonprojectte--581102906"
     key    = "state/hackaton-cicd-deploy"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
